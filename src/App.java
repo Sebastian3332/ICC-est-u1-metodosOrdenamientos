83e4ef455adceb2466549f18d9ec16a1ab9d6b89
Your Name <you@example.com>
2026-04-20 21:00:34 -0500

    Metodos de Ordenamiento

--- src/App.java
+++ src/App.java
@@ -1,12 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
+        metodoBurbuja();
     }
 
     public static void metodoBurbuja() {
         System.out.println("Metodo Burbuja");
+
+        int[] arreglo = new int[]{10, -5, 0, 2, 7};
         MetodoBurbuja metodoBurbuja = new MetodoBurbuja(arreglo);
-        int[] arreglo = new int[]{10,-5,0,2,7};
+
+        // Original
+        metodoBurbuja.imprimirArreglo();
+
+        // Ascendente
+        metodoBurbuja.ordenarAscendente();
+        metodoBurbuja.imprimirArreglo();
+
+        // Descendente
+        metodoBurbuja.ordenarDescendente();
         metodoBurbuja.imprimirArreglo();
     }
 }
--- src/MetodoBurbuja.java
+++ src/MetodoBurbuja.java
@@ -1,37 +1,41 @@
 public class MetodoBurbuja {
 
-
     int[] arreglo;
     
     // Constructor
-    public MetodoBurbuja(){
-            System.out.println("Se creo la clase Met Burbuja");
-            // 1ra forma
-            // Instanciar con valor creado en constructor
-            // this.arreglo = new int[]{10, 5, 8, 0, 7};
-
-            // 2da forma
-            // Instanciar con valor creado en constructor
-            this.arreglo = arreglo;
-
+    public MetodoBurbuja(int[] arreglo){
+        System.out.println("Se creo la clase Met Burbuja");
+        this.arreglo = arreglo;
     }
+
     public void ordenarAscendente() {
-            for(int i = 0; i<arreglo.length; i++){
-                for(int j = i+1; j<arreglo.length; j++){
-                    if(arreglo[i] > arreglo[j]){
-                        int aux = arreglo[i];
-                        arreglo[i] = arreglo[j];
-                        arreglo[j] = aux;
-                    }
+        for(int i = 0; i < arreglo.length; i++){
+            for(int j = i + 1; j < arreglo.length; j++){
+                if(arreglo[i] > arreglo[j]){
+                    int aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = aux;
                 }
             }
+        }
     }
 
+    public void ordenarDescendente() {
+        for(int i = 0; i < arreglo.length; i++){
+            for(int j = i + 1; j < arreglo.length; j++){
+                if(arreglo[i] < arreglo[j]){ 
+                    int aux = arreglo[i];
+                    arreglo[i] = arreglo[j];
+                    arreglo[j] = aux;
+                }
+            }
+        }
+    }
 
     public void imprimirArreglo(){
         for(int num : arreglo){
-            System.out.println(num + ", ");    
+            System.out.print(num + ", ");    
         }
         System.out.println();
-    }
+    } 
 }
