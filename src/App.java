fca9f867d4e09fd5ef15d19af30cc327e6a11d64
Your Name <you@example.com>
2026-04-20 20:41:27 -0500

    Primer commit del proyecto Java

--- src/App.java
+++ src/App.java
@@ -4,6 +4,9 @@ public class App {
     }
 
     public static void metodoBurbuja() {
-            
+        System.out.println("Metodo Burbuja");
+        MetodoBurbuja metodoBurbuja = new MetodoBurbuja(arreglo);
+        int[] arreglo = new int[]{10,-5,0,2,7};
+        metodoBurbuja.imprimirArreglo();
     }
 }
--- src/MetodoBurbuja.java
+++ src/MetodoBurbuja.java
@@ -1,8 +1,37 @@
 public class MetodoBurbuja {
+
+
+    int[] arreglo;
     
     // Constructor
     public MetodoBurbuja(){
+            System.out.println("Se creo la clase Met Burbuja");
+            // 1ra forma
+            // Instanciar con valor creado en constructor
+            // this.arreglo = new int[]{10, 5, 8, 0, 7};
 
-        
+            // 2da forma
+            // Instanciar con valor creado en constructor
+            this.arreglo = arreglo;
+
+    }
+    public void ordenarAscendente() {
+            for(int i = 0; i<arreglo.length; i++){
+                for(int j = i+1; j<arreglo.length; j++){
+                    if(arreglo[i] > arreglo[j]){
+                        int aux = arreglo[i];
+                        arreglo[i] = arreglo[j];
+                        arreglo[j] = aux;
+                    }
+                }
+            }
+    }
+
+
+    public void imprimirArreglo(){
+        for(int num : arreglo){
+            System.out.println(num + ", ");    
+        }
+        System.out.println();
     }
 }
